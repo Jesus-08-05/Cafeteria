dc9573ddd9591551e312dfebbd23103e426e43b5
Trevor-Velmont <montesgomezyoiner@gmail.com>
2024-05-22 17:14:19 +0200

    catalogo

--- app.py
+++ app.py
@@ -8,21 +8,21 @@ app.config.from_object('config.Config')
 def index():
     return render_template('index.html')
 
-<<<<<<< HEAD
+
 @app.route('/quienes')
 def quienes():
     return render_template('quienes.html')
 
-@app.route('/inicio')
-def inicio():
-    return render_template('inicio.html')
-=======
 
 @app.route('/catalogo')
 def catalogo():
     return render_template('catalogo.html')
 
->>>>>>> 1545e04ca7aa2b3e39dcde839131cf3a7d24e847
+
+@app.route('/inicio')
+def inicio():
+    return render_template('inicio.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
