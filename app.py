1545e04ca7aa2b3e39dcde839131cf3a7d24e847
Trevor-Velmont <montesgomezyoiner@gmail.com>
2024-05-22 16:05:02 +0200

    configuracuon vista catalogo

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config.from_object('config.Config')
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/catalogo')
+def catalogo():
+    return render_template('catalogo.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,3 @@
+class Config:
+    DEBUG = True
+    SECRET_KEY = '12345'
