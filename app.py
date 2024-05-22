1f411b81983810d22a8bad1b0396fdf0c048741f
Jesus-08-05 <vergaraguitierantoniojesus@gmail.com>
2024-05-22 10:04:23 -0500

    Meteme cacho pero no me dejes mi amor

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/quienes')
+def quienes():
+    return render_template('quienes.html')
+
+@app.route('/inicio')
+def inicio():
+    return render_template('inicio.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
